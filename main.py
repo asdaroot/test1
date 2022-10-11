bd89fbe2763687b8e768296d919490d48846182d
asdaroot <asdaroot@gmail.com>
2022-10-11 00:04:59 +0000

    Add github action V1

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 n = int(input())
-
 numerics = []
-
 for _ in range(n):
     number = int(input())
     numerics.append(number)
-
 a = int(input())
-    b = int(input())
-
+b = int(input())
 print(sum(numerics[a-1:b]))
